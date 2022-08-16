04afdace5d5da9774c3c413d2f53303be96b51c0
sanjay2334 <sanjay9894151755@gmail.com>
2022-08-16 22:20:35 +0530

    git commit -m 'login comp'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Metamask from './pages/metamask'
 import Bg from './assets/hospitalbg3.jpg'
 import {BrowserRouter as Router,Routes ,Route} from 'react-router-dom'
 import Install from './pages/install';
+import Login from './pages/login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Metamask />} />
           <Route path='/install' element={<Install />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+const Login = () => {
+    return ( 
+        <div className="flex flex-row">
+        <div className="w-1/2 h-screen  flex items-end justify-end"></div>
+        <div className="bg-white w-1/2 h-[60rem] flex m-3 rounded-md backdrop-blur-md bg-white/30">
+            <div className="flex justify-center w-full flex-colum">
+                <h1 className="text-5xl mt-4">LOGIN</h1>
+                {/* <div className="justify-center">
+                <input type='text' />
+            </div> */}
+            </div>
+        </div>
+        </div>
+     );
+}
+ 
+export default Login;
